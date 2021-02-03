b5a04b0d14ebcc7bdf25f452be6aac67e67ccf0b
jcbumanglag <51851580+jcbumanglag@users.noreply.github.com>
2021-02-04 02:05:42 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace Sorting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = "Bubble Sort";
+        }
+         
+  
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Equals("") || textBox1.Text.Equals(null))
+            {
+                MessageBox.Show("Invalid Input");
+            }
+            else
+
+            {
+              bubblesort b = new bubblesort();
+              char[] j = textBox1.Text.ToCharArray();
+              label1.Text = "Sorted: "+ String.Join(" ",b.studs(j));
+             
+            }
+            
+
+       
+        }
+    }
+}
---
+++ bubblesort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sorting
+{
+    public class bubblesort
+    {
+        public char[] studs(char[] v)
+        {
+            for (int i = 0; i < v.Length - 1; i++)
+            {
+                for (int k = 0; k < v.Length - i - 1; k++)
+                {
+                    if (v[k] > v[k + 1])
+                    {
+                        char temp = v[k];
+                        v[k] = v[k + 1];
+                        v[k + 1] = temp;
+                    }
+                }
+                
+            }
+            return v;
+        } 
+    }
+}
